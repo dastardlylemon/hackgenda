a64dc9376391131de135241b52a080ea98550716
Aman Agarwal <aman.agarwal.2718@gmail.com>
2014-02-15 15:31:30 -0500

    android build

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+/home/aman/Projects/hackgenda/platforms/android/bin/classes.dex : \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/BuildConfig.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/Hackgenda.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/R$drawable.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/R$string.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/R$xml.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/R.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/classes/com/phonegap/hackgenda/R$attr.class \
+/home/aman/Projects/hackgenda/platforms/android/bin/dexedLibs/classes-c584d601ee8a9dbf219883808b44ec37.jar \
