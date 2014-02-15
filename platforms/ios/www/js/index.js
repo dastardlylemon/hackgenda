412c7a2be31168ca09aebe2013c09ef6140197b2
dastardlylemon <jwu@efunda.com>
2014-02-15 03:12:56 -0500

    fb/twitter login

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,10 +15,13 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        if (parseFloat(window.device.version) === 7.0) {
+            document.body.style.marginTop = "20px";
+        }
+        //app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    /*receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -45,5 +30,31 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    }*/
 };
+
+var chatRef = new Firebase('https://hackgenda.firebaseio.com');
+var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+                                   if (error) {
+                                   // an error occurred while attempting login
+                                   console.log(error);
+                                   } else if (user) {
+                                   // user authenticated with Firebase
+                                   console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+                                   } else {
+                                   // user is logged out
+                                   }
+                                   });
+
+function fbLogin() {
+    auth.login('facebook', {
+               rememberMe: true,
+               scope: 'email'
+               });
+}
+
+function twLogin() {
+    auth.login('twitter', {
+               rememberMe: true
+               });
+}
